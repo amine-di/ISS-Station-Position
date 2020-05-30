b89ecc843299fa66bc15e32a31c1d37c824a9ce5
Amine <amine.aboutaieb@gmail.com>
2020-05-30 23:16:29 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var map = L.map('map').setView([51.505, 0], 3);
+var lat = 51.5;
+var long = -0.09;
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+
+
+
+window.setInterval(() => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let obj = JSON.parse(xhr.response);
+            console.log(obj);
+            map.setView([obj.iss_position.latitude, obj.iss_position.longitude], 5);
+            L.circle([obj.iss_position.latitude, obj.iss_position.longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: 100
+            }).addTo(map);
+
+        }
+    }
+    xhr.open('GET', 'http://api.open-notify.org/iss-now.json', true);
+    xhr.send();
+}, 1000);
